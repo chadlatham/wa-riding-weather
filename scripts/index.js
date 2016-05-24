a2fcf6444437a39e8728d34597ce77f99661dd8e
Chad Latham <chadlatham33@gmail.com>
2016-05-24 16:32:50 -0700

    Proving APIs

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,8 @@
 // http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID={APIKEY}
+// http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}
 // API KEY: 4241da6fa29994783519776c66246929
+
+// {"_id":5805687,"name":"Olympia","country":"US","coord":{"lon":-122.900703,"lat":47.037868}}
 
 // Do not send requests more then 1 time per 10 minutes from one device/one API key. Normally the weather is not changing so frequently.
 
@@ -8,3 +11,25 @@
 // 3 Call API by city ID instead of city name, city coordinates or zip code. In this case you get precise respond exactly for your city.
 
 // 4 Free account has limitation of capacity and data availability. If you do not get respond from server do not try to repeat your request immediately, but only after 10 min. Also we recommend to store your previous request data.
+
+
+
+const apiKey = '4241da6fa29994783519776c66246929';
+const url = 'http://api.openweathermap.org/data/2.5/';
+let cityId = '5788054'
+let lon = -122.900703;
+let lat = 47.037868;
+let timeFrame = 'forecast'
+let method = '/city'
+
+// To search by city code
+// let query = `${url}${timeFrame}/${method}?id=${cityId}&APPID=${apiKey}`;
+
+// To search by latitude and longitude
+let query = `${url}${timeFrame}?APPID=${apiKey}&lat=${lat}&lon=${lon}&units=imperial`
+// let queryString = 'http://api.openweathermap.org/data/2.5/forecast?APPID=4241da6fa29994783519776c66246929&lat=47.037868&lon=-122.900703'
+
+$xhr = $.getJSON(query);
+$xhr.done((data) => {
+  console.log(data);
+});
