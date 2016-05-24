df5260eebc05d8e858edc2bf3d027d3c13d0d4c6
Chad Latham <chadlatham33@gmail.com>
2016-05-24 10:02:32 -0700

    Notes on OpenWeatherMap API

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+// http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID={APIKEY}
+// API KEY: 4241da6fa29994783519776c66246929
+
+// Do not send requests more then 1 time per 10 minutes from one device/one API key. Normally the weather is not changing so frequently.
+
+// 2 Use the name of the server as api.openweathermap.org. Please never use the IP address of the server.
+
+// 3 Call API by city ID instead of city name, city coordinates or zip code. In this case you get precise respond exactly for your city.
+
+// 4 Free account has limitation of capacity and data availability. If you do not get respond from server do not try to repeat your request immediately, but only after 10 min. Also we recommend to store your previous request data.
